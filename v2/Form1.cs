a8c5dda276afd46ef690d5f0926369225f7eff08
Devrouete <corentin.devrouete@viacesi.fr>
2020-01-27 16:53:52 +0100

    First Commit with project with xml test

---
+++ v2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Dialog 
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "XML Files (*.xml)|*.xml";
+            dialog.FilterIndex = 0;
+            dialog.DefaultExt = "xml";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+
+                SetText(dialog.FileName);
+
+                try
+                {
+                    var file = new StreamReader(dialog.FileName);
+                    
+                }
+
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Secutiry error. \n\n Error message: {ex.Message} \n\n" + $"Details: \n\n{ex.StackTrace}");
+                }
+
+                if (!string.Equals(Path.GetExtension(dialog.FileName),
+                        ".xml",
+                        System.StringComparison.OrdinalIgnoreCase
+                    ))
+                {
+
+                    MessageBox.Show("The type of the selected file is not supported by this application. You must select an XML file.",
+                        "Invalid File Type",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+
+
+                }
+
+            }
+            else { }
+
+            
+
+        }
+
+        private void SetText(string text)
+        {
+            textBox1.Text = text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
